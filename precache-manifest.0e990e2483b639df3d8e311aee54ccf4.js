d2f258d474b82ecb77088e9c277c6e01f841ba9c
Diwan,Naman <Naman.Diwan@cerner.com>
2020-06-01 17:01:50 +0530

    Updates

---
+++ precache-manifest.0e990e2483b639df3d8e311aee54ccf4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3606fe7e1b2691cefed833f0d5a498b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "caa85be2a6fe09f76915",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "041d51fc58336d7bb02c",
+    "url": "/Portfolio/static/css/main.cca97797.chunk.css"
+  },
+  {
+    "revision": "caa85be2a6fe09f76915",
+    "url": "/Portfolio/static/js/2.bfeaec23.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.bfeaec23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "041d51fc58336d7bb02c",
+    "url": "/Portfolio/static/js/main.fafc9b23.chunk.js"
+  },
+  {
+    "revision": "369b31d766fd42c8e84c",
+    "url": "/Portfolio/static/js/runtime-main.16c280df.js"
+  },
+  {
+    "revision": "676f954b77225a99939884db709bee0a",
+    "url": "/Portfolio/static/media/Cerner.676f954b.png"
+  },
+  {
+    "revision": "66c14cccbf199b6754eef5dcd1d613e0",
+    "url": "/Portfolio/static/media/DPS.66c14ccc.jpg"
+  },
+  {
+    "revision": "6e7f45e7d28409bcc54c08b3abff0c39",
+    "url": "/Portfolio/static/media/FIS.6e7f45e7.png"
+  },
+  {
+    "revision": "d90afc808c4f5a881b3b7b5055e9fc9f",
+    "url": "/Portfolio/static/media/Powergrid.d90afc80.jpg"
+  },
+  {
+    "revision": "982463593623d5979ab716ba3158c12f",
+    "url": "/Portfolio/static/media/Resume.98246359.pdf"
+  },
+  {
+    "revision": "d4f564df83002d1b7548a3f8fbd1e3ca",
+    "url": "/Portfolio/static/media/Web.d4f564df.jpg"
+  },
+  {
+    "revision": "b47d1d73d88b81f77693d7169c6e800d",
+    "url": "/Portfolio/static/media/Web2.b47d1d73.jpg"
+  },
+  {
+    "revision": "3338d2d8702e8c07f9be093f8f2b4146",
+    "url": "/Portfolio/static/media/Web3.3338d2d8.jpg"
+  }
+]);
